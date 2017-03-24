dcae9cc948cfaf84631b22205179a95cc2fe14e5
BenEmdon <benjaminemdon@gmail.com>
2017-03-24 18:43:41 -0400

    Added more info

--- Pixel Art Maker.playground/Pages/Info.xcplaygroundpage/Contents.swift
+++ Pixel Art Maker.playground/Pages/Info.xcplaygroundpage/Contents.swift
@@ -1,2 +1,27 @@
-//: ![title](Pixel-art-maker.jpg)
-//: [Next](@next)
+/*: 
+![title](Pixel-art-maker.jpg)
+[👉 Demo](@next)
+
+# About:
+
+
+# Features:
+### Drawing 🖌
+![Drawing](Drawing.gif)
+You can either do single clicks to change pixels or you can click and drag!
+### Changing Color 🎨
+![ChangingColor](ChangingColor.gif)
+Give the `CanvasController` an array of colors (`[UIColor]`) to setup your pallet.
+### Undo and Redo ↪️ ↩️
+![UndoRedo](UndoRedo.gif)
+Hit the undo and redo buttons to undo and redo draws.
+### Save 💾
+![Save](Save.gif)
+Hitting the save button will make a pretty render of your art without the grid and save it.
+
+### NOTES ABOUT SAVING ⚠️
+In order to save your art you must ensure the following directory exists:
+
+`/Users/{YOUR USER NAME}/Documents/Shared\ Playground\ Data`
+
+*/
--- Pixel Art Maker.playground/Pages/PixelArtMakerDemo.xcplaygroundpage/Contents.swift
+++ Pixel Art Maker.playground/Pages/PixelArtMakerDemo.xcplaygroundpage/Contents.swift
@@ -2,15 +2,14 @@ import UIKit
 import PlaygroundSupport
 
 //: ![mario](Mario.jpg)
-//: ### COOL
 let marioColorSet = [#colorLiteral(red: 0.9260939956, green: 0.7393692136, blue: 0.535525322, alpha: 1), #colorLiteral(red: 0.8486657143, green: 0.1457175612, blue: 0.1098935679, alpha: 1), #colorLiteral(red: 0.03515542299, green: 0.0390829891, blue: 0.04739325494, alpha: 1), #colorLiteral(red: 0.2316169739, green: 0.252183795, blue: 0.6651875973, alpha: 1), #colorLiteral(red: 0.5066443086, green: 0.2547130287, blue: 0.1472603977, alpha: 1), #colorLiteral(red: 0.8298331499, green: 0.7703962922, blue: 0.1196306571, alpha: 1)]
 
-let benColorSet = [#colorLiteral(red: 1, green: 0.8869820837, blue: 0.7137309097, alpha: 1), #colorLiteral(red: 0.5702336431, green: 0.7469733357, blue: 0.8845283389, alpha: 1), #colorLiteral(red: 0.03515542299, green: 0.0390829891, blue: 0.04739325494, alpha: 1), #colorLiteral(red: 0.1563676894, green: 0.1678637564, blue: 0.2093632221, alpha: 1), #colorLiteral(red: 0.5075009424, green: 0.2946772792, blue: 0.1519830821, alpha: 1), #colorLiteral(red: 0.9870286584, green: 0.8794633746, blue: 0.7908019423, alpha: 1), #colorLiteral(red: 0.7835152745, green: 0.3807955086, blue: 0.4221667051, alpha: 1), #colorLiteral(red: 0.6703479405, green: 0.3896099235, blue: 0.2029382737, alpha: 1),]
+let appleLogoColorSet = [#colorLiteral(red: 0.9319646955, green: 0.1334255934, blue: 0.1326510012, alpha: 1), #colorLiteral(red: 0.2898080647, green: 0.03988443315, blue: 0.03802879527, alpha: 1), #colorLiteral(red: 0.9332934022, green: 0.5342146754, blue: 0.2673675418, alpha: 1), #colorLiteral(red: 0.4786225557, green: 0.2711006403, blue: 0.1341979504, alpha: 1), #colorLiteral(red: 0.9492279887, green: 0.7960495353, blue: 0.4377158582, alpha: 1), #colorLiteral(red: 0.5441896915, green: 0.4260497093, blue: 0.1513271332, alpha: 1), #colorLiteral(red: 0.6013671756, green: 0.7315731645, blue: 0.1352686286, alpha: 1), #colorLiteral(red: 0.1738204658, green: 0.2733283043, blue: 0.001983167371, alpha: 1), #colorLiteral(red: 0.2009291649, green: 0.4018282294, blue: 0.6674943566, alpha: 1), #colorLiteral(red: 0.09352166206, green: 0.1855295897, blue: 0.3100581169, alpha: 1), #colorLiteral(red: 0.3591453135, green: 0.2413133681, blue: 0.3027350307, alpha: 1), #colorLiteral(red: 0.1978913844, green: 0.09638027102, blue: 0.1455260813, alpha: 1)]
 
 let pixelSize: CGFloat = 15
-let heightInPixels: Int = 31
-let canvasDefaultColor: UIColor = #colorLiteral(red: 1, green: 0.6332477699, blue: 0.7159463751, alpha: 1)
-let colors: [UIColor] = benColorSet
+let heightInPixels: Int = 20
+let canvasDefaultColor: UIColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+let colors: [UIColor] = appleLogoColorSet
 
 let path = playgroundSharedDataDirectory.appendingPathComponent("Ben.jpg")
 
